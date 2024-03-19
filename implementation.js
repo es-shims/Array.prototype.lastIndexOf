7c013c3cd598deae3815fdb6f21076073c728337
Jordan Harband <ljharb@gmail.com>
2024-03-19 10:54:51 -0700

    [Deps] update `call-bind`, `define-properties`, `es-abstract`, `is-negative-zero`

--- implementation.js
+++ implementation.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var Get = require('es-abstract/2023/Get');
-var HasProperty = require('es-abstract/2023/HasProperty');
-var ToIntegerOrInfinity = require('es-abstract/2023/ToIntegerOrInfinity');
-var ToLength = require('es-abstract/2023/ToLength');
+var Get = require('es-abstract/2024/Get');
+var HasProperty = require('es-abstract/2024/HasProperty');
+var ToIntegerOrInfinity = require('es-abstract/2024/ToIntegerOrInfinity');
+var ToLength = require('es-abstract/2024/ToLength');
 var ToObject = require('es-object-atoms/ToObject');
-var ToString = require('es-abstract/2023/ToString');
+var ToString = require('es-abstract/2024/ToString');
 
 var callBound = require('call-bind/callBound');
 var isNegativeZero = require('is-negative-zero');
