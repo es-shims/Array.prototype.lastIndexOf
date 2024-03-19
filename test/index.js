80e8cd82f7c9fc28fb92ee91bea552d571d690ac
Jordan Harband <ljharb@gmail.com>
2024-03-19 10:50:32 -0700

    [Tests] use `call-bind` instead of `function-bind`

--- test/index.js
+++ test/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var lastIndexOf = require('..');
-var bind = require('function-bind');
+var callBind = require('call-bind');
 var test = require('tape');
 var runTests = require('./tests');
 
 test('as a function', function (t) {
 	t.test('bad array/this value', function (st) {
-		st['throws'](bind.call(lastIndexOf, null, undefined, 'a'), TypeError, 'undefined is not an object');
-		st['throws'](bind.call(lastIndexOf, null, null, 'a'), TypeError, 'null is not an object');
+		st['throws'](callBind(lastIndexOf, null, undefined, 'a'), TypeError, 'undefined is not an object');
+		st['throws'](callBind(lastIndexOf, null, null, 'a'), TypeError, 'null is not an object');
 		st.end();
 	});
 
